032dc226dc279c886b0451d90bc471c1f9a015f6
Gourav <singhgourav008@gmail.com>
2025-07-29 17:01:16 +0530

    Initial Commit

---
+++ android/app/src/main/java/com/demo/capacitor/MainActivity.java
@@ -0,0 +1,5 @@
+package com.demo.capacitor;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <header className="main-header">
+            <h1>My Capacitor App</h1>
+          </header>
+        </div>
+        <nav className="nav-link">
+          <Link to="/">Home</Link> | <Link to="/about">About</Link> |{' '}
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
